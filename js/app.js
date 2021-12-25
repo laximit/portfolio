a6e86527aa26988932f419412a65dc39d3980fc6
Murilo Lobos <murilolobos47@gmail.com>
2021-12-25 15:16:21 -0300

    dark mode done, transition var added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 const dropdownButton = document.getElementById('dropdown-button');
 const dropdownMenu = document.getElementById('dropdown-menu');
+const themeButton = document.getElementById('theme-button');
+const body = document.body;
+
+const theme = localStorage.getItem('theme');
+
+if (theme) {
+	body.classList.add(theme);
+}
 
 dropdownButton.onclick = () => {
 	dropdownMenu.classList.toggle('active');
+}
+
+function changeTheme() {
+	if (body.classList.contains('light')) {
+		body.classList.replace('light', 'dark');
+		localStorage.setItem('theme', 'dark');
+	} else if (body.classList.contains('dark')) {
+		body.classList.replace('dark', 'light');
+		localStorage.setItem('theme', 'light');
+	}
 }
